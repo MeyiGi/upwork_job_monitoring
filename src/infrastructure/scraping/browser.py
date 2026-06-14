b7421d01c4da6f65db873995d4fd4627cf704241
MeyiGi <faybmi7@gmail.com>
2026-06-14 14:02:09 +0600

    chore: update daily stats, normalize quote style in browser.py

--- src/infrastructure/scraping/browser.py
+++ src/infrastructure/scraping/browser.py
@@ -9,12 +9,12 @@ class Browser:
     def __init__(self, cookies_file: str):
         self._cookies_file = cookies_file
         co = ChromiumOptions()
-        # co.set_argument('--headless')
+        # co.set_argument("--headless")
         # co.set_argument('--no-sandbox')
         # co.set_argument('--disable-dev-shm-usage')
         # co.set_argument('--disable-gpu')
         # co.set_argument('--remote-allow-origins=*')
-        co.set_browser_path('/usr/bin/google-chrome')
+        co.set_browser_path("/usr/bin/google-chrome")
 
         self.instance = Chromium(co)
         self.tab = self.instance.new_tab()
